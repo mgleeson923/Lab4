7fefb77e6de64522384e48b3b4ecefadb6d1cc12
mgleeson923 <mgleeson923@gmail.com>
2017-06-29 08:32:48 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //Calculate the factorial of a number
+
+        Scanner scnr = new Scanner(System.in);
+        int userNum = 0;
+
+        System.out.println("Let's calculate Factorials!");
+
+        System.out.print("Please input an integer from 1 to 10: ");
+        userNum = scnr.nextInt();
+
+        
+    }
+}
